d8394f6bcf7b9327912487ff7c88507e71d66fe0
adrianababilonia22 <72059895+adrianababilonia22@users.noreply.github.com>
2020-10-27 13:46:43 -0500

    Create ejemplos.py

---
+++ ejemplos.py
@@ -0,0 +1,35 @@
+# OPERACIONES EN LISTAS
+# ------------------------
+
+lista1 = [1]
+lista2 = lista1
+lista1[0] = 2
+print(lista2)
+
+# ------------------------
+# RODAJAS Y del()
+
+#miLista = [10, 8, 6, 4, 2]
+#del miLista[1:3]
+#print(miLista)
+
+#del miLista[:]
+#print(miLista)
+
+#del miLista
+#print(miLista)
+
+# ------------------------
+# OPERADORES in / not in
+
+#miLista = [0, 3, 12, 8, 2]
+
+#print(5 in miLista)
+#print(5 not in miLista)
+#print(12 in miLista)
+
+# ------------------------
+# COMPRENSIÓN DE LISTA
+
+#cuadrados = [x ** 2 for x in range(10)]
+#impares = [x for x in cuadrados if x % 2 != 0]
