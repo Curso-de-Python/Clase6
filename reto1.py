98fdd9abf93c3240f1659360492ddc420925820f
adrianababilonia22 <72059895+adrianababilonia22@users.noreply.github.com>
2020-10-27 13:48:01 -0500

    Create reto1.py

---
+++ reto1.py
@@ -0,0 +1,17 @@
+'''
+-----------------------------
+ RETO N°1
+ Operando con listas
+-----------------------------
+Escribe un programa que elimine todas las repeticiones de números de una lista. Asume que la lista ya se encuentra dentro del código y no necesitas preguntarle los datos al usuario.
+
+ SUGERENCIA: Te recomendamos que crees una nueva lista como área de trabajo temporal, no necesitas actualizar la lista actual.
+-----------------------------
+'''
+
+miLista = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
+
+# coloca tu código aquí
+
+print("La lista solo con elementos únicos:")
+print(listaUnica)
